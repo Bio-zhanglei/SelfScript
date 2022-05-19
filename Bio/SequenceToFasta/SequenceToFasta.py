1d185f0fe8879d52b214054ab94fd4b00d4ed3d4
Bio-zhanglei <65288445+Bio-zhanglei@users.noreply.github.com>
2022-05-19 10:03:58 +0800

    Update SequenceToFasta.py

--- Bio/SequenceToFasta/SequenceToFasta.py
+++ Bio/SequenceToFasta/SequenceToFasta.py
@@ -4,11 +4,20 @@
 file = 'SequenceData.txt'
 ID_symbol = '>'
 
+#读取数据
 with open(file,'r') as f1:
   sequence_data = f1.read().splitlines()
 
-#获取id
+#获取id,替换@符号为>符号
 sequence_id = sequence_data[0::4]
+sequence_id = [ID_symbol+i[1:] for i in sequence_id[:]]
 
 #获取测序read
 sequence_read = sequence_data[1::4]
+
+#转为fasta文件格式
+fasta_format = sum(list(zip(sequence_id,sequence_read)),())
+
+#输出文件
+with open('fasta_{0}'.format(file),'w') as f1:
+  f1.write('\n'.join(fasta_format))

e3159283026091084afc46279d8668364e557c27
Bio-zhanglei <65288445+Bio-zhanglei@users.noreply.github.com>
2022-05-19 09:40:59 +0800

    Update SequenceToFasta.py

--- Bio/SequenceToFasta/SequenceToFasta.py
+++ Bio/SequenceToFasta/SequenceToFasta.py
@@ -2,3 +2,13 @@
 ###同时每个read的ID的@符号转为>符号(可选)
 
 file = 'SequenceData.txt'
+ID_symbol = '>'
+
+with open(file,'r') as f1:
+  sequence_data = f1.read().splitlines()
+
+#获取id
+sequence_id = sequence_data[0::4]
+
+#获取测序read
+sequence_read = sequence_data[1::4]
